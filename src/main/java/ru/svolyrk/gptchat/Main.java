809e85eeab3c45c75497532fc05294ffbfeb9a51
Maxim.Krilov <maxim.krilov704@gmail.com>
2024-06-08 00:08:08 +0300

    add app Logging

--- src/main/java/ru/svolyrk/gptchat/Main.java
+++ src/main/java/ru/svolyrk/gptchat/Main.java
@@ -1,12 +1,36 @@
 package ru.svolyrk.gptchat;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+@Slf4j
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        loggingAppInfo();
         SpringApplication.run(Main.class, args);
+    }
+
+    private static void loggingAppInfo() {
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader("pom.xml"));
+            log.info("-------------------------------------");
+            log.info(model.getId());
+            log.info(model.getGroupId());
+            log.info(model.getArtifactId());
+            log.info(model.getVersion());
+            log.info("-------------------------------------");
+        } catch (IOException | XmlPullParserException e) {
+            log.error("Error reading pom.xml", e);
+        }
     }
 }
